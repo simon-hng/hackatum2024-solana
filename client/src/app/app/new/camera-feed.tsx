6aae28a04395bea4d9cc9fb68115866962dd765a
simon huang <simon.huang@outlook.de>
2024-11-23 17:59:16 +0100

    add bet draft form

--- client/src/app/app/new/camera-feed.tsx
+++ client/src/app/app/new/camera-feed.tsx
@@ -8,13 +8,44 @@ import {
 import { AnimatePresence, motion } from "motion/react";
 import React from "react";
 import { useRef, useEffect } from "react";
-import { Button } from "~/components/ui/button";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { env } from "~/env";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/components/ui/form";
+import { Textarea } from "@components/ui/textarea";
+
+import { Input } from "@components/ui/input";
 import { cn } from "~/lib/utils";
+import { Button } from "~/components/ui/button";
 
 const deepgram = createClient(env.NEXT_PUBLIC_DEEPGRAM_API_KEY);
 
 export const CameraFeed = () => {
+  const betSchema = z.object({
+    challenger: z.string(),
+    challenged: z.string(),
+    title: z.string(),
+    amount: z.number(),
+  });
+
+  const form = useForm<z.infer<typeof betSchema>>({
+    resolver: zodResolver(betSchema),
+    defaultValues: {
+      challenger: "me",
+      challenged: "",
+      title: "",
+      amount: 0,
+    },
+  });
+
   let microphone: MediaRecorder;
   const beginAudioRecording = async (connection: ListenLiveClient) => {
     microphone = await navigator.mediaDevices
@@ -102,45 +133,76 @@ export const CameraFeed = () => {
   const [text, setText] = React.useState("");
 
   return (
-    <div className="animate relative flex h-screen flex-col overflow-hidden p-2 pb-20">
-      <video
-        className={cn("h-full rounded-lg object-cover")}
-        playsInline
-        ref={myVideoRef}
-        autoPlay
-      />
+    <Form {...form}>
+      <form className="animate relative flex h-screen flex-col overflow-hidden p-2 pb-20">
+        <video
+          className={cn("h-full rounded-lg object-cover")}
+          playsInline
+          ref={myVideoRef}
+          autoPlay
+        />
 
-      <AnimatePresence>
-        {isRecording && (
-          <motion.div
-            initial={{ height: 0 }}
-            animate={{ height: "32rem" }}
-            exit={{ height: 0 }}
-          >
-            {text}
-          </motion.div>
-        )}
-      </AnimatePresence>
+        <AnimatePresence>
+          {isRecording && (
+            <motion.div
+              initial={{ height: 0 }}
+              animate={{ height: "32rem" }}
+              exit={{ height: 0 }}
+            >
+              <FormField
+                control={form.control}
+                name="challenged"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>I bet</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Elon Musk" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
 
-      <div className="fixed bottom-24 left-1/2 -translate-x-1/2">
-        <div
-          className={cn(
-            "border-border transform rounded-full border-2 p-1 duration-200",
-            {
-              "bg-background/20 border-red-500 p-4": isRecording,
-            },
+              <FormField
+                control={form.control}
+                name="challenged"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>to</FormLabel>
+                    <FormControl>
+                      <Textarea placeholder="Catch a rocket" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              {text}
+            </motion.div>
           )}
-        >
-          <Button
-            className={cn("h-16 w-16 rounded-full", {
-              "border-red-500 bg-red-500 active:bg-red-500": isRecording,
-            })}
-            variant="outline"
-            onTouchStart={() => startRecording()}
-            onTouchEnd={() => stopRecording()}
-          />
+        </AnimatePresence>
+
+        <div className="fixed bottom-24 left-1/2 -translate-x-1/2">
+          <div
+            className={cn(
+              "border-border transform rounded-full border-2 p-1 duration-200",
+              {
+                "bg-background/20 border-red-500 p-4": isRecording,
+              },
+            )}
+          >
+            <Button
+              type="button"
+              className={cn("h-16 w-16 rounded-full", {
+                "hover:bg-red-500 border-red-500 bg-red-500 active:bg-red-500":
+                  isRecording,
+              })}
+              variant="outline"
+              onTouchStart={() => startRecording()}
+              onTouchEnd={() => stopRecording()}
+            />
+          </div>
         </div>
-      </div>
-    </div>
+      </form>
+    </Form>
   );
 };
